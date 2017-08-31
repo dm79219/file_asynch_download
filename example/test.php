30af1cdb3897143f10ea1dada6f2f1ff4dcb114a
dm79219 <divyanshumahajan1@gmail.com>
2017-08-31 23:21:50 +0530

    example for using this library

---
+++ example/test.php
@@ -0,0 +1,8 @@
+<?php 
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+
+$obj = new Library();
+$sql = "select * from <table_name>"; // Query here
+$response = $obj->run($sql);
+print_r($response);
